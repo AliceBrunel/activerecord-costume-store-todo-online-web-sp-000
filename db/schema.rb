a5c867bae7cb28f197d58edaf21c9264b2e60e1e
Alice Brunel <alc.brunel@gmail.com>
2019-09-18 20:33:03 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "costume", force: :cascade do |t|
     t.string "name"
@@ -20,15 +20,6 @@ ActiveRecord::Schema.define(version: 2) do
     t.boolean "still_in_business"
     t.datetime "start_time"
     t.datetime "end_time"
-  end
-
-  create_table "costumes", force: :cascade do |t|
-    t.string "name"
-    t.float "price"
-    t.integer "size"
-    t.string "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
