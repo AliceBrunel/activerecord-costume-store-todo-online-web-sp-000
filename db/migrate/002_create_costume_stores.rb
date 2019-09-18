16a693146488f692d115ced2e2a15ac7ff78b50c
Alice Brunel <alc.brunel@gmail.com>
2019-09-18 20:02:10 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.string :costume_inventory
-      t.integer :size
+      t.integer :num_of_employees
+      t.string :
       t.timestamps :created_at 
       t.timestamps :updated_at
     end
