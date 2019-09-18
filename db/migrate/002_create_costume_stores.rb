b9df583344cdceb48403cb10539c1ce6c476ded7
Alice Brunel <alc.brunel@gmail.com>
2019-09-18 20:00:53 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,14 @@
 # Create your costume_stores migration here
 
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
-  
+  def change
+    create_table :costume do |t|
+      t.string :name
+      t.float :location
+      t.string :image_url
+      t.integer :size
+      t.timestamps :created_at 
+      t.timestamps :updated_at
+    end
+  end
 end
