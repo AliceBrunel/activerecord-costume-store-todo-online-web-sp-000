e2c115c0d35cc4536294e79ed0f57ed13807be88
Alice Brunel <alc.brunel@gmail.com>
2019-09-18 20:01:16 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume do |t|
       t.string :name
-      t.float :location
-      t.string :image_url
+      t.string :location
+      t.string :
       t.integer :size
       t.timestamps :created_at 
       t.timestamps :updated_at
