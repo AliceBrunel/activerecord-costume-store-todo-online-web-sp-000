95109fdeb71b603bf7be128dae41e654e3ece9b9
Alice Brunel <alc.brunel@gmail.com>
2019-09-18 20:18:46 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,12 +2,12 @@
 
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     def change
-    create_table :costume_stores do |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.string :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
+      t.string :theme
+      t.integer :price
+      t.boolean :
       t.datetime :start_time
       t.datetime :end_time
     end
